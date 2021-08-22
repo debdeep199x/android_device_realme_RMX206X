5854acb677979e28f77701ce55c11046845cace3
debdeep98 <80145521+debdeep98@users.noreply.github.com>
2021-08-22 11:50:47 +0530

    Update and rename ssos_RMX206X.mk to wave_RMX206X.mk

--- wave_RMX206X.mk
+++ wave_RMX206X.mk
@@ -6,18 +6,18 @@
 
 $(call inherit-product, device/realme/RMX206X/device.mk)
 
-# Inherit some common SSOS stuff.
+# Inherit some common wave stuff.
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_WITH_GAPPS := true
-$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
+$(call inherit-product, vendor/wave/configs/common.mk)
 
 # All components inherited here go to system_ext image
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := ssos_RMX206X
+PRODUCT_NAME := wave_RMX206X
 PRODUCT_DEVICE := RMX206X
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme 6 Pro
